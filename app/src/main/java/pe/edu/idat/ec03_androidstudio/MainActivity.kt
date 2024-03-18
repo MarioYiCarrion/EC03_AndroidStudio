f004aeea28814fac4a468ebc23ade9145fa969b7
MarioYiCarrion <147116987+MarioYiCarrion@users.noreply.github.com>
2024-03-17 22:46:34 -0500

    Update MainActivity.kt

--- app/src/main/java/pe/edu/idat/ec03_androidstudio/MainActivity.kt
+++ app/src/main/java/pe/edu/idat/ec03_androidstudio/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.listadoFragment
+                R.id.principalFragment, R.id.formularioFragment, R.id.listadoFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
